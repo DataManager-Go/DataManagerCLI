c51a7544735101bf749282387ba10c8d30b5edfa
JojiiOfficial <jojii@jojii.de>
2020-04-05 15:38:26 +0200

    migrate namespace commands to libdm

--- commands/Attribute.go
+++ commands/Attribute.go
@@ -2,72 +2,10 @@ package commands
 
 import (
 	"fmt"
-	"log"
 
 	libdm "github.com/DataManager-Go/libdatamanager"
-	"github.com/Yukaru-san/DataManager_Client/models"
-	"github.com/Yukaru-san/DataManager_Client/server"
 	"github.com/fatih/color"
 )
-
-// do an attribute request (update/delete group or tag). action: 0 - delete, 1 - update
-func attributeRequest(cData CommandData, attribute models.Attribute, action uint8, name string, newName ...string) {
-	var endpoint server.Endpoint
-
-	// Pick right endpoint
-	if action == 1 {
-		if attribute == models.GroupAttribute {
-			endpoint = server.EPGroupUpdate
-		} else {
-			endpoint = server.EPTagUpdate
-		}
-	} else if action == 0 {
-		if attribute == models.GroupAttribute {
-			endpoint = server.EPGroupDelete
-		} else {
-			endpoint = server.EPTagDelete
-		}
-	}
-
-	// Build request
-	request := server.UpdateAttributeRequest{
-		Name:      name,
-		Namespace: cData.Namespace,
-	}
-
-	// Add new name on update request
-	if action == 1 {
-		request.NewName = newName[0]
-	}
-
-	response, err := server.NewRequest(endpoint, request, cData.Config).WithAuth(server.Authorization{
-		Type:    server.Bearer,
-		Palyoad: cData.Config.User.SessionToken,
-	}).WithBenchCallback(cData.BenchDone).Do(nil)
-
-	// Error handling #1
-	if err != nil {
-		if response != nil {
-			fmt.Println("http:", response.HTTPCode)
-			return
-		}
-		log.Fatalln(err)
-	}
-
-	// Error handling #2
-	if response.Status == server.ResponseError {
-		printResponseError(response)
-		return
-	}
-
-	// Output
-	actionString := "updated"
-	if action == 0 {
-		actionString = "deleted"
-	}
-
-	fmt.Printf("The attribute has been %s\n", color.HiGreenString("successfully "+actionString))
-}
 
 // UpdateAttribute update an attribute
 func UpdateAttribute(cData CommandData, attribute libdm.Attribute, name, newName string) {
@@ -87,5 +25,6 @@ func DeleteAttribute(cData CommandData, attribute libdm.Attribute, name string) 
 		fmt.Println(err)
 		return
 	}
+
 	fmt.Printf("The attribute has been %s\n", color.HiGreenString("successfully deleted"))
 }
--- commands/Namespace.go
+++ commands/Namespace.go
@@ -2,137 +2,57 @@ package commands
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/JojiiOfficial/DataManagerServer/constants"
-	"github.com/Yukaru-san/DataManager_Client/models"
-	"github.com/Yukaru-san/DataManager_Client/server"
 )
 
-func handleNamespaceCommand(cData CommandData, action uint8, name, newName string, customNs bool) {
-	//Get correct endpoint
-	endpoint := namespaceActionToEndpoint(action)
-
-	nsType := models.UserNamespaceType
-	if customNs {
-		nsType = models.CustomNamespaceType
-	}
-
-	var res server.StringResponse
-
-	response, err := server.NewRequest(endpoint, server.NamespaceRequest{
-		Namespace: name,
-		NewName:   newName,
-		Type:      nsType,
-	}, cData.Config).WithAuth(server.Authorization{
-		Type:    server.Bearer,
-		Palyoad: cData.Config.User.SessionToken,
-	}).WithBenchCallback(cData.BenchDone).Do(&res)
-
-	// Error handling #1
-	if err != nil {
-		if response != nil {
-			fmt.Println("http:", response.HTTPCode)
-			return
-		}
-		log.Fatalln(err)
-	}
-
-	// Error handling #2
-	if response.Status == server.ResponseError {
-		printResponseError(response, namespaceActionToString(action, false, true))
-	} else {
-		if cData.OutputJSON {
-			fmt.Println(toJSON(res))
-		} else {
-			switch action {
-			case 2, 0:
-				fmt.Printf("%s %s namespace '%s'\n", constants.GreenSuccessfully, namespaceActionToString(action, true, false), name)
-			case 1:
-				fmt.Printf("%s created namespace '%s'\n", constants.GreenSuccessfully, res.String)
-			}
-		}
-	}
-}
-
-//CreateNamespace creates a namespace
+// CreateNamespace creates a namespace
 func CreateNamespace(cData CommandData, name string, customNS bool) {
-	handleNamespaceCommand(cData, 1, name, "", customNS)
-}
-
-//UpdateNamespace update a namespace
-func UpdateNamespace(cData CommandData, name, newName string, customNS bool) {
-	handleNamespaceCommand(cData, 2, name, newName, false)
-}
-
-//DeleteNamespace update a namespace
-func DeleteNamespace(cData CommandData, name string) {
-	handleNamespaceCommand(cData, 0, name, "", false)
-}
-
-//ListNamespace lists your namespace
-func ListNamespace(cData CommandData) {
-	var resp server.StringSliceResponse
-
-	response, err := server.NewRequest(server.EPNamespaceList, nil, cData.Config).WithAuth(server.Authorization{
-		Type:    server.Bearer,
-		Palyoad: cData.Config.User.SessionToken,
-	}).WithBenchCallback(cData.BenchDone).Do(&resp)
-
+	createResponse, err := cData.LibDM.CreateNamespace(name, customNS)
 	if err != nil {
-		if response != nil {
-			fmt.Println("http:", response.HTTPCode)
-			return
-		}
-		log.Fatalln(err)
+		fmt.Println(err)
+		return
 	}
 
-	if response.Status == server.ResponseError {
-		printResponseError(response, "listing namespaces")
+	fmt.Printf("%s created namespace '%s'\n", constants.GreenSuccessfully, createResponse.String)
+}
+
+// UpdateNamespace update a namespace
+func UpdateNamespace(cData CommandData, name, newName string, customNS bool) {
+	updateResponse, err := cData.LibDM.UpdateNamespace(name, newName, customNS)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s updated namespace '%s'\n", constants.GreenSuccessfully, updateResponse.String)
+}
+
+// DeleteNamespace update a namespace
+func DeleteNamespace(cData CommandData, name string) {
+	deleteResponse, err := cData.LibDM.DeleteNamespace(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s deleted namespace '%s'\n", constants.GreenSuccessfully, deleteResponse.String)
+}
+
+// ListNamespace lists your namespace
+func ListNamespace(cData CommandData) {
+	getNamespaceResponse, err := cData.LibDM.GetNamespaces()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if cData.OutputJSON {
+		fmt.Println(toJSON(getNamespaceResponse))
 	} else {
-		if cData.OutputJSON {
-			fmt.Println(toJSON(resp))
-		} else {
-			fmt.Println("Your namespaces:")
-			for _, namespace := range resp.Slice {
-				fmt.Println(namespace)
-			}
+		fmt.Println("Your namespaces:")
+		for _, namespace := range getNamespaceResponse.Slice {
+			fmt.Println(namespace)
 		}
 	}
-}
-
-func namespaceActionToString(action uint8, past, pp bool) (name string) {
-	switch action {
-	case 0:
-		name = "delete"
-	case 1:
-		name = "create"
-	case 2:
-		name = "update"
-	}
-
-	if pp {
-		name += "ing"
-		return
-	}
-
-	if past {
-		name += "d"
-		return
-	}
-
-	return
-}
-
-func namespaceActionToEndpoint(action uint8) (endpoint server.Endpoint) {
-	switch action {
-	case 0:
-		endpoint = server.EPNamespaceDelete
-	case 1:
-		endpoint = server.EPNamespaceCreate
-	case 2:
-		endpoint = server.EPNamespaceUpdate
-	}
-
-	return
 }
