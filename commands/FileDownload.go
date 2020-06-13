42b781082edea0fda2a88821be52d3164ecac115
JojiiOfficial <jojii@jojii.de>
2020-06-13 13:49:38 +0200

    add new progressbar for filedownload

--- commands/FileDownload.go
+++ commands/FileDownload.go
@@ -2,14 +2,17 @@ package commands
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/DataManager-Go/libdatamanager"
 	libdm "github.com/DataManager-Go/libdatamanager"
 	"github.com/JojiiOfficial/gaw"
+	"github.com/fatih/color"
 )
 
 // DownloadData information for downloading files
@@ -18,6 +21,8 @@ type DownloadData struct {
 	FileID    uint
 	LocalPath string
 	Preview   bool
+
+	ProgressView *ProgressView
 }
 
 // ViewFile view file
@@ -36,7 +41,7 @@ func (cData *CommandData) ViewFile(downloadData *DownloadData) {
 		defer ShredderFile(tmpFile, -1)
 
 		// Write file
-		if err = cData.writeFile(resp, tmpFile, nil); err != nil {
+		if err = cData.writeFile(resp, tmpFile, nil, nil); err != nil {
 			return
 		}
 
@@ -69,8 +74,20 @@ func (cData *CommandData) DownloadFile(downloadData *DownloadData) error {
 	// Do request but don't read the body yet
 	resp, err := downloadData.doRequest(cData, true)
 	if err != nil {
-		printResponseError(err, "downloading file")
+		printResponseError(err, "requesting file")
 		return err
+	}
+
+	var bar *Bar
+	if !cData.Quiet {
+		// Create new progressview
+		if downloadData.ProgressView == nil {
+			downloadData.ProgressView = NewProgressView()
+		}
+
+		// Create and add bar
+		bar = NewBar(DownloadTask, resp.Size, resp.ServerFileName, false)
+		downloadData.ProgressView.AddBar(bar)
 	}
 
 	// Determine where the file should be stored in
@@ -88,7 +105,7 @@ func (cData *CommandData) DownloadFile(downloadData *DownloadData) error {
 
 	go func() {
 		// Save server file to local 'outFile'
-		if err = cData.writeFile(resp, outFile, cancel); err != nil {
+		if err = cData.writeFile(resp, outFile, cancel, bar); err != nil {
 			// Delete file on error. On checksum error only delete if --verify was passed
 			if err != libdm.ErrChecksumNotMatch || cData.VerifyFile {
 				ShredderFile(outFile, -1)
@@ -103,19 +120,40 @@ func (cData *CommandData) DownloadFile(downloadData *DownloadData) error {
 
 	// Wait for download to be done or delete file on interrupt
 	awaitOrInterrupt(c, func(s os.Signal) {
+		if bar != nil {
+			bar.stop("Cancelled. Erasing file!")
+		}
+
 		cancel <- true
 
 		// await shredder
 		<-c
 		os.Exit(1)
 	}, func(s string) {
+		var text string
+
 		if len(s) > 0 {
-			// printBar(fmt.Sprintf("%s %s: %s", color.HiRedString("Error"), "downloading file", s), data.bar)
-			os.Exit(1)
+			text = fmt.Sprintf("%s %s: %s", color.HiRedString("Error"), "downloading file", s)
 		} else {
-			// printBar(sPrintSuccess("saved '%s'", outFile))
+			text = fmt.Sprintf("saved '%s'", outFile)
+		}
+
+		// Print text
+		if bar == nil {
+			fmt.Println(text)
+		} else {
+			bar.doneTextChan <- text
 		}
 	})
+
+	if downloadData.ProgressView != nil {
+		// Wait for bars to complete
+		for i := range downloadData.ProgressView.RawBars {
+			for !downloadData.ProgressView.RawBars[i].done {
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+	}
 
 	return nil
 }
@@ -141,14 +179,33 @@ func (cData *CommandData) handleDecryption(resp *libdm.FileDownloadResponse) {
 	}
 }
 
-func (cData *CommandData) writeFile(resp *libdm.FileDownloadResponse, file string, cancel chan bool) error {
+// Write response to a given file
+func (cData *CommandData) writeFile(resp *libdm.FileDownloadResponse, file string, cancel chan bool, bar *Bar) error {
+	if bar != nil {
+		resp.DownloadRequest.ReaderProxy = func(r io.Reader) io.Reader {
+			// Proxy reader through bar
+			return bar.bar.ProxyReader(r)
+		}
+	}
+
 	// Save file to tempFile
 	if err := resp.WriteToFile(file, 0600, cancel); err != nil {
+		// Make error readable
+		var errText string
 		if err == libdm.ErrChecksumNotMatch {
-			// printBar(cData.getChecksumError(resp))
+			errText = cData.getChecksumError(resp)
 		} else {
-			// printBar(fmt.Sprintf("%s %s: %s", color.HiRedString("Error"), "downloading file", err.Error()))
+			errText = getError("downloading file", err.Error())
 		}
+
+		// View the error
+		if bar != nil {
+			bar.doneTextChan <- errText
+		} else {
+			fmt.Println(errText)
+		}
+
+		return err
 	}
 
 	return nil
@@ -192,6 +249,9 @@ func (cData *CommandData) downloadFiles(files []libdm.FileResponseItem, outDir s
 
 	totalfiles := len(files)
 
+	// Use this context for progressview
+	progressView := NewProgressView()
+
 	// Start Downloader pool
 	for pos < totalfiles {
 		read := <-c
@@ -214,9 +274,10 @@ func (cData *CommandData) downloadFiles(files []libdm.FileResponseItem, outDir s
 
 				// Download file
 				err := cData.DownloadFile(&DownloadData{
-					FileName:  file.Name,
-					FileID:    file.ID,
-					LocalPath: filepath.Join(rootDir, dir),
+					FileName:     file.Name,
+					FileID:       file.ID,
+					LocalPath:    filepath.Join(rootDir, dir),
+					ProgressView: progressView,
 				})
 
 				if err != nil {
